175f2e62181e086f27b1165c8ce0b271042e4d71
Nathan Reed <reednj@gmail.com>
2010-05-26 10:37:14 +0800

    Restructuring of source tree  + config file now kept locally and deployed  + added cgi directives to .htaccess needed for eruby on prod  + removed db sync: now running the data collector on prod, so there is no reason for db syncing.

--- script/deploy.sh
+++ script/deploy.sh
@@ -6,7 +6,9 @@ cp -R ../web/* ~/tmpcpy/
 cp -R ../shared/*.rb ~/tmpcpy/lib/
 
 # remove the config file, and other files we don't want to copy
+# rename the prod config file to that gets deployed
 rm ~/tmpcpy/lib/so-config.rb
+mv ~/tmpcpy/lib/so-config.prod.rb ~/tmpcpy/lib/so-config.rb
 
 scp -r ~/tmpcpy/* reednj@popacular.com:~/popacular.com/analyticsoverflow/
 
