bd814da13301ec297e3b9348cc017f9337082279
Nathan Reed <reednj@gmail.com>
2010-05-27 11:28:19 +0800

    Added created_date to Tag table

--- script/db-create.sql
+++ script/db-create.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Apr 30, 2010 at 10:09 AM
+-- Generation Time: May 27, 2010 at 11:27 AM
 -- Server version: 5.0.51
 -- PHP Version: 5.2.4-2ubuntu5.10
 
@@ -22,6 +22,7 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 CREATE TABLE IF NOT EXISTS `Tag` (
   `tag_id` int(11) NOT NULL auto_increment,
   `tag_name` varchar(128) character set latin1 NOT NULL,
+  `created_date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`tag_id`),
   UNIQUE KEY `tag_name` (`tag_name`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
@@ -38,5 +39,6 @@ CREATE TABLE IF NOT EXISTS `TagValue` (
   `tag_value` double NOT NULL,
   `created_date` timestamp NOT NULL default CURRENT_TIMESTAMP,
   PRIMARY KEY  (`value_id`),
-  UNIQUE KEY `tag_id` (`tag_id`,`created_date`)
+  UNIQUE KEY `tag_id` (`tag_id`,`created_date`),
+  KEY `created_date` (`created_date`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8;
