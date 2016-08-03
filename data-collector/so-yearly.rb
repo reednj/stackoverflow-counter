0d177f035333cb926693df0bb567d0186cdf84a9
Nathan Reed <reednj@gmail.com>
2016-08-03 09:09:56 +0800

    methods to load and save the son data to a file

--- data-collector/so-yearly.rb
+++ data-collector/so-yearly.rb
@@ -6,26 +6,34 @@ require 'rubygems'
 require 'time'
 require 'mysql2'
 require 'sequel'
+require 'json'
 
 require 'so-config'
 
 class App
 	def main
-		@db = Sequel.connect({
+		data = get_top_tags_for_year(2015)
+		
+	end
+
+	def db
+		@db ||= Sequel.connect({
 			:adapter => 'mysql2',
 			:host => $DB_HOST,
 			:username => $DB_USER,
 			:password => $DB_PASS,
 			:database => $DB_NAME
 		})
+	end
 
-		data = get_top_tags_for_year(2015)
-		puts data
+	def load_json(path)
+		JSON.parse File.read(path), :symbolize_names => true
 	end
 
 	def get_top_tags_for_year(year)
-		start_date = Time.new year, 1, 1
-		end_date = Time.new year+1, 1, 1
+		start_date = Time.new(year, 1, 1)
+		end_date = Time.new(year + 1, 1, 1)
+
 		query = "select 
 			t.tag_name,
 			t.description,
@@ -40,7 +48,19 @@ class App
 		order by (max(tag_value) - min(tag_value)) desc
 		limit 20"
 
-		@db[query, start_date, end_date].all
+		db[query, start_date, end_date].all
+	end
+end
+
+class Hash
+	def save_json(path)
+		File.open(path, 'w') { |file| file.write self.to_json }
+	end
+end
+
+class Array
+	def save_json(path)
+		File.open(path, 'w') { |file| file.write self.to_json }
 	end
 end
 
