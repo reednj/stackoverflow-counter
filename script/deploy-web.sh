2874d181fc186ad6125f16c44d3ee0c9fa148ab6
Nathan Reed <reednj@gmail.com>
2016-08-17 11:47:24 +0800

    update deployment script

--- script/deploy-web.sh
+++ script/deploy-web.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-ssh reednj@popacular.com "rm -rf ~/so.reednj.com/*"
-scp -r ../web/* reednj@popacular.com:~/so.reednj.com/
-scp -r ../shared reednj@popacular.com:~/so.reednj.com/lib
+HOST=reednj@paint.reednj.com 
 
-ssh reednj@popacular.com "cp ~/code/config_backup/so/so-config.rb ~/so.reednj.com/lib/"
-ssh reednj@popacular.com "cd ~/so.reednj.com; ln /home/reednj/scripts/yearly-tags.json"
-ssh reednj@popacular.com "mkdir ~/so.reednj.com/tmp; touch ~/so.reednj.com/tmp/restart.txt"
+ssh $HOST "rm -rf ~/so.reednj.com/*"
+scp -r ../web/* $HOST:~/so.reednj.com/
+scp -r ../shared $HOST:~/so.reednj.com/lib
+
+ssh $HOST "cp ~/code/config_backup/so/so-config.rb ~/so.reednj.com/lib/"
+#ssh $HOST "cd ~/so.reednj.com; ln /home/reednj/scripts/yearly-tags.json"
+ssh $HOST "mkdir ~/so.reednj.com/tmp; touch ~/so.reednj.com/tmp/restart.txt"
