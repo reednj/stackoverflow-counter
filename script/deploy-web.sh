025e576cf650f1fe48e34d1dc099d12061abc983
Nathan Reed <reednj@gmail.com>
2016-08-17 11:55:00 +0800

    update deployment script to pull down yearly stats

--- script/deploy-web.sh
+++ script/deploy-web.sh
@@ -7,5 +7,5 @@ scp -r ../web/* $HOST:~/so.reednj.com/
 scp -r ../shared $HOST:~/so.reednj.com/lib
 
 ssh $HOST "cp ~/code/config_backup/so/so-config.rb ~/so.reednj.com/lib/"
-#ssh $HOST "cd ~/so.reednj.com; ln /home/reednj/scripts/yearly-tags.json"
+ssh $HOST "scp popacular.com:so.reednj.com/*.json ~/so.reednj.com"
 ssh $HOST "mkdir ~/so.reednj.com/tmp; touch ~/so.reednj.com/tmp/restart.txt"
