9e32193a930397630c06eebdbf40b161b43b9eec
Nathan Reed <reednj@gmail.com>
2016-08-19 11:42:00 +0800

    update deployment script

--- script/deploy-web.sh
+++ script/deploy-web.sh
@@ -3,8 +3,7 @@
 HOST=reednj@paint.reednj.com 
 
 ssh $HOST "rm -rf ~/so.reednj.com/*"
-scp -r ../web/* $HOST:~/so.reednj.com/
-scp -r ../shared $HOST:~/so.reednj.com/lib
+scp -r ../* $HOST:~/so.reednj.com/
 
 ssh $HOST "cp ~/code/config_backup/so/so-config.rb ~/so.reednj.com/lib/"
 ssh $HOST "scp popacular.com:so.reednj.com/*.json ~/so.reednj.com"
