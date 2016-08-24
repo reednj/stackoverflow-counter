b929af4bd6e730954e700fd1328ab0201798b529
Nathan Reed <reednj@gmail.com>
2016-08-24 09:37:44 +0800

    update deployment script

--- script/deploy-web.sh
+++ script/deploy-web.sh
@@ -6,5 +6,5 @@ ssh $HOST "rm -rf ~/so.reednj.com/*"
 scp -r ../* $HOST:~/so.reednj.com/
 
 ssh $HOST "cp ~/code/config_backup/so/so-config.rb ~/so.reednj.com/lib/"
-ssh $HOST "scp popacular.com:so.reednj.com/*.json ~/so.reednj.com"
+ssh $HOST "cd ~/so.reednj.com; rm yearly-tags.json; ln ~/yearly-tags.json"
 ssh $HOST "mkdir ~/so.reednj.com/tmp; touch ~/so.reednj.com/tmp/restart.txt"
