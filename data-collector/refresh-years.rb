67fdafaab61b08e8dbb5035d43c4e017fa9fa1e7
Nathan Reed <reednj@gmail.com>
2017-02-22 11:27:44 +0800

    new script to refresh entire yearly list

---
+++ data-collector/refresh-years.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+chdir "data-collector" unless File.exist? 'so-yearly.rb'
+system 'rm ../yearly-tags.json'
+
+(2009..Time.now.year).each do |year|
+    system "./so-yearly.rb #{year}"
+end
